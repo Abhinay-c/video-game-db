96e81a75adf2bed72694f3770a4b719379930c94
Abhinay Challa <abhinayc365@gmail.com>
2025-06-27 15:54:00 +0530

    add home component feature

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  public sort: String = '';
 }
